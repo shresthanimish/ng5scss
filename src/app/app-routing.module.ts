508e3801b6bcee269a5e72691c7a8a5fa6b2176f
nimish <shrestha.nimish@gmail.com>
2017-11-23 21:57:35 +1100

    Code clean up.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { TaskComponent } from './task/task.component';
-import {BijayaComponent} from "./bijaya/bijaya.component";
-import {RetailersComponent} from "./retailers/retailers.component";
 import {PostsComponent} from "./posts/posts.component";
 
 const routes: Routes = [
   { path: 'posts', component: PostsComponent },
-  { path: 'task', component: TaskComponent },
-  { path: 'bijaya', component: BijayaComponent },
-  { path: 'retailers', component: RetailersComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,12 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { TaskComponent } from './task/task.component';
-import { BijayaComponent } from './bijaya/bijaya.component';
-import { RetailersComponent } from './retailers/retailers.component';
 import { PostsComponent } from './posts/posts.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent,
-    BijayaComponent,
-    RetailersComponent,
     PostsComponent
   ],
   imports: [
